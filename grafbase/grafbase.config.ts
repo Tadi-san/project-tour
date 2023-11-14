8b22f0640bcef5138423319e800d629b0dc04a14
Tadi-san <tadiosanegagregn@yahoo.com>
2023-11-14 10:50:14 +0300

    fixing for grafbase

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,26 +1,29 @@
-import { g, auth ,config } from '@grafbase/sdk'
+import { g, config, auth } from '@grafbase/sdk';
 
+// @ts-ignore
 const User = g.model('User', {
-  name:g.string().length({min:2, max:20}),
-  email:g.string().unique(),
-  avatarUrl:g.url(),
-  description:g.string().optional(),
-  githubUrl:g.url().optional(),
-  linkedinUrl:g.url().optional(),]
-  projects: g.relation(()=>Project).list().optional(),
+  name: g.string().length({ min: 2, max: 100 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(), 
+  projects: g.relation(() => Project).list().optional(),
 })
 
+// @ts-ignore
 const Project = g.model('Project', {
-  title:g.string().length({min:3}),
-  description:g.string(),
-  image:g.url(),
-  githubUrl:g.url(),
-  catagory:g.string().search(),
-  createdBy:g.relation(()=> User)
+  title: g.string().length({ min: 3 }),
+  description: g.string(), 
+  image: g.url(),
+  liveSiteUrl: g.url(), 
+  githubUrl: g.url(), 
+  category: g.string().search(),
+  createdBy: g.relation(() => User),
 })
+
+
 
 export default config({
   schema: g,
-
-  },
-  )
+})
